5fdf007878c22a553555e62c31db2d5da0962deb
tobias <tobias.omondi@student.moringaschool.com>
2025-05-10 11:41:21 +0300

    landing page

--- chickenshopfiy/src/Components/HomePage/LandingPage.jsx
+++ chickenshopfiy/src/Components/HomePage/LandingPage.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import Navbar from '../../Navbar'
+import React from 'react';
 import chickenfries from '/src/assets/chickenlandingpage.jpeg';
+import Navbar from '../../Navbar';
+import '../../App.css'
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className='relative w-screen '>
-        <img src={chickenfries} alt='chicken fried' className='w-full brightness-75 object-cover' />
+    <div className='relative w-full h-screen -mt-20'>
+      {/* Background Image */}
+      <img
+        src={chickenfries}
+        alt='chicken fried'
+        className='absolute inset-0 w-full h-full object-cover brightness-75'
+      />
+
+      {/* Navbar on top */}
+      <div className='absolute top-0 left-0 w-full z-20'>
+        <Navbar />
       </div>
-      <div className='z-20 absolute top-1/4 mx-auto'>
-      <Navbar />
-      </div>
-      <div className='z-20 absolute top-1/2 left-10 '>
-        <h1 className='text-7xl font-extrabold mt-50'>CHICKEN SHOPIFY</h1>
-        <h4 className='text-4xl font-bold mt-5'>For Chicken Livers <br/>Only</h4>
+
+      {/* Page Content Centered */}
+      <div className='absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2 z-20 text-start text-black'>
+        <h1 className='text-7xl font-extrabold mt-28'>CHICKEN SHOPIFY</h1>
+        <h4 className='text-4xl font-bold mt-5'>
+          For Chicken Livers <br /> Only
+        </h4>
+        <button type='submit' className='bg-black opacity-75 text-white rounded-2xl p-4 mt-10'>Shop Today</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
--- chickenshopfiy/src/Navbar.jsx
+++ chickenshopfiy/src/Navbar.jsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { IoMdCart } from "react-icons/io";
-
 
 const Navbar = () => {
   return (
-      <div className=' bg-amber-800 opacity-75 rounded-2xl p-3 w-5xl mx-auto relative '>
-        <nav className='text-white flex  justify-end'>
-          <ul className='list-none flex flex-row justify-center gap-12'>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
+    <nav className='flex justify-end p-7 bg-transparent'>
+      <ul className='list-none flex flex-row justify-center gap-12 text-white'>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
 
-            <li>
-              <Link to="/vendor-store">Vender Store</Link>
-            </li>
+        <li>
+          <Link to="/vendor-store">Vendor Store</Link>
+        </li>
 
-            <li>
-              <Link to="/help">Help</Link>
-            </li>
+        <li>
+          <Link to="/help">Help</Link>
+        </li>
 
-            <li>
-              <Link to="/contact-us">ContactUs</Link>
-            </li>
+        <li>
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
 
-            <li>
-              <Link to="/Sign-in">Signin</Link>
-            </li>
+        <li>
+          <Link to="/sign-in">Signin</Link>
+        </li>
 
-            <li>
-              <Link to="/cart"><IoMdCart size={20}/></Link>
-            </li>
-            
-          </ul>
-        </nav>
-    </div>
-  )
-}
+        <li>
+          <Link to="/cart">
+            <IoMdCart size={20} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
