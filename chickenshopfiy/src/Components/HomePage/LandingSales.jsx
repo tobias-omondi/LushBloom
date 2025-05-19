f44c4908b4d82d29ef932b3b036c038aab432d4e
tobias <tobias.omondi@student.moringaschool.com>
2025-05-19 13:39:05 +0300

    new design

--- chickenshopfiy/src/Components/HomePage/LandingSales.jsx
+++ chickenshopfiy/src/Components/HomePage/LandingSales.jsx
@@ -8,8 +8,8 @@ const LandingSales = ({Title, Description}) => {
   return (
     <div className='bg-orange-500 w-full rounded-t-4xl p-4 md:p-10'>
       <div>
-        <h1 className='text-white text-3xl md:text-5xl py-2 md:py-5 font-bold'>The one chicken behind it all</h1>
-        <h3 className='text-gray-900 py-4 md:py-3 text-xl md:text-4xl'>Sell online and in person. Sell fresh or Frozen.<span className='cursor-pointer hover:text-gray-700'>sell locally and nationwide.</span> <br/> <span className='cursor-pointer hover:text-gray-700'>Sell direct or wholesale.</span><span className='cursor-pointer hover:text-gray-600'> Sell on mobile or desktop.</span></h3>
+        <h1 className='text-white text-3xl md:text-5xl py-2 md:py-5 font-bold'>The one beauty behind it all</h1>
+        <h3 className='text-gray-900 py-4 md:py-3 text-xl md:text-4xl'>Sell online and in person.frozen in time.<span className='cursor-pointer hover:text-gray-700'>sell locally and nationwide.</span> <br/> <span className='cursor-pointer hover:text-gray-700'>Sell direct or wholesale.</span><span className='cursor-pointer hover:text-gray-600'> Sell on mobile or desktop.</span></h3>
       </div>
 
       <div className='flex flex-col md:flex-row items-center justify-center gap-4 md:gap-8 object-cover'>
