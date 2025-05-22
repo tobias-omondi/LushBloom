247486675e9fe81ef391fdc57e2207cba228a579
tobias <tobias.omondi@student.moringaschool.com>
2025-05-22 21:17:00 +0300

    added a grid loyalt

--- Lushbloombeauty/src/Components/HomePage/LandingScroll.jsx
+++ Lushbloombeauty/src/Components/HomePage/LandingScroll.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import skincare1 from '/src/assets/skincare1.jpeg'
+import skincare2 from '/src/assets/skincare2.jpeg'
+import skincare3 from '/src/assets/skincare3.jpeg'
 
 const LandingScroll = () => {
   return (
-    <div>
-      <h1 className='text-white'>kmdkdk</h1>
+    <div className='mt-16 flex justify-evenly items-center '>
+      <img src={skincare1} alt='skincare' className='w-[350px] h-[400px] object-cover rounded-3xl'/>
+      <img src={skincare2} alt='skincare' className='w-[350px] h-[400px] object-cover rounded-3xl'/>
+      <img src={skincare3} alt='skincare' className='w-[350px] h-[400px] object-cover rounded-3xl'/>
     </div>
   )
 }
