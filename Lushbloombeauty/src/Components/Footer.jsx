5ae428b3df10142c23a737825be24e7fbf0c754a
tobias <tobias.omondi@student.moringaschool.com>
2025-06-02 11:32:25 +0300

    changed landing image from videos to picture

--- Lushbloombeauty/src/Components/Footer.jsx
+++ Lushbloombeauty/src/Components/Footer.jsx
@@ -5,8 +5,11 @@ const Footer = () => {
   return (
     <>
     <div className='footer-section'>
-      <h1 className='text-white text-3xl text-center font-bold'>Description will be here about our page</h1>
+      <h1 className='text-white text-sm md:text-xl text-center md:w-2/3 md:mx-auto font-medium'>LushBloom is a beauty marketplace for vendors to sell their products online. Join our community and grow your brand with us.</h1>
     </div>
+
+    {/* main footer container */}
+    
     <div className='bg-black p-10'>
       <h2 className='text-white '>footer section</h2>
     </div>
