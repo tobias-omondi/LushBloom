649e59bb1c6202eb517780623b237288721e33e5
tobias <tobias.omondi@student.moringaschool.com>
2025-06-17 13:00:30 +0300

    Testing responsivness

--- Lushbloombeauty/src/Components/HomePage/SectionTwo.jsx
+++ Lushbloombeauty/src/Components/HomePage/SectionTwo.jsx
@@ -5,7 +5,7 @@ import LandingScroll from './LandingScroll'
 const SectionTwo = () => {
   return (
     <>
-    <div className='mt-12 flex flex-col md:flex-row justify-between bg-blue-500 items-center w-full'>
+    <div className='mt-12 flex flex-col md:flex-row justify-evenly  items-center w-full p-10 shadow'>
         <div className='w-full md:w-2/4'>
             <h1 className='text-gray-950 text-2xl  md:text-3xl py-2 md:py-5 font-medium text-center '>From small teams to large organizations</h1>
             <p className='text-center mx-auto w-4/5 text-black '>Discover a platform crafted for beauty visionaries of every kind. Whether you're launching your first skincare line or managing a thriving salon, we give you the tools to grow, sell, and shine. From bold indie brands to luxury cosmetics labels, we help bring your beauty business to life online and in person. Seamlessly showcase your products, connect with your audience, and make every interaction unforgettable. Because in beauty, every detail matters and so does your brand.</p>
