deb4066a637f707ed83acd930b1a7ecdeb6f4787
tobias <tobias.omondi@student.moringaschool.com>
2025-07-01 13:55:04 +0300

    revamped style

--- Lushbloombeauty/src/Components/VenderPage/VenderSectionFour.jsx
+++ Lushbloombeauty/src/Components/VenderPage/VenderSectionFour.jsx
@@ -5,6 +5,12 @@ import sliderthree from "/src/assets/vendersliderimages/24 Moisturizing Products
 import sliderfour from "/src/assets/vendersliderimages/nails.jpeg";
 import sliderfive from "/src/assets/vendersliderimages/beautyshop.jpeg";
 
+import sliderSix from "/src/assets/vendersliderimages/felipepelaquim-ywE0IlojSI8-unsplash.jpg";
+import sliderseven from "/src/assets/vendersliderimages/kimia-zarifi-x4J_92kJBoY-unsplash.jpg";
+import slidereight from "/src/assets/vendersliderimages/nika-benedictova-6AEZRIVkfT0-unsplash.jpg";
+import slidernine from "/src/assets/vendersliderimages/package.jpeg";
+import sliderten from "/src/assets/vendersliderimages/Treat Your Haircare Routine Like Your Skincare Routine.jpeg";
+
 import { FaStar, FaHeart } from "react-icons/fa";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
@@ -20,91 +26,96 @@ const showOneSlides = [
   { id: 5, image: sliderfive },
 ];
 
+const showTwoSlides = [
+  { id: 1, image: sliderSix },
+  { id: 2, image: sliderseven },
+  { id: 3, image: slidereight },
+  { id: 4, image: slidernine },
+  { id: 5, image: sliderten },
+];
+
 const VenderSectionFour = () => {
   return (
     <>
-    <div className="p-6">
-      <Swiper
-        spaceBetween={20}
-        slidesPerView={3}
-        loop={true}
-        autoplay={{
-          delay: 6000, // 3 seconds between slides
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-      >
-        {showOneSlides.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className="flex flex-col items-center">
-              <img
-                src={slide.image}
-                alt={`Slide ${slide.id}`}
-                className="object-cover rounded-lg shadow-md w-4/5 h-[300px]"
-              />
-              <div className="flex flex-row gap-20 mt-4">
-
-                <div className='flex flex-row'>
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className='text-gray-400'/>
+      <div className="px-4 sm:px-6 py-8">
+        <Swiper
+          spaceBetween={16}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {showOneSlides.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div className="flex flex-col items-center max-w-screen-md mx-auto">
+                <img
+                  src={slide.image}
+                  alt={`Slide ${slide.id}`}
+                  className="object-cover rounded-lg shadow-md w-full h-[200px] sm:h-[250px] md:h-[300px]"
+                />
+                <div className="flex justify-between items-center w-full mt-3 px-2">
+                  <div className="flex">
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-gray-400" />
+                  </div>
+                  <FaHeart size={20} className="text-red-600" />
                 </div>
-             
-              <div>
-              <FaHeart size={20} className="text-red-600" />
               </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
-              </div>
-            
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-
-
-     {/* part two of slider */}
-    <div className="p-6">
-      <Swiper
-        spaceBetween={20}
-        slidesPerView={3}
-        loop={true}
-        autoplay={{
-          delay: 6000, // 3 seconds between slides
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-      >
-        {showOneSlides.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className="flex flex-col items-center">
-              <img
-                src={slide.image}
-                alt={`Slide ${slide.id}`}
-                className="object-cover rounded-lg shadow-md w-4/5 h-[300px]"
-              />
-              <div className="flex flex-row gap-20 mt-4">
-
-                <div className='flex flex-row'>
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-orange-500" />
-                <FaStar size={20} className="text-gray-400"/>
-                <FaStar size={20} className='text-gray-400'/>
+      {/* Second Slider */}
+      <div className="px-4 sm:px-6 py-8">
+        <Swiper
+          spaceBetween={16}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {showTwoSlides.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div className="flex flex-col items-center max-w-screen-md mx-auto">
+                <img
+                  src={slide.image}
+                  alt={`Slide ${slide.id}`}
+                  className="object-cover rounded-lg shadow-md w-full h-[200px] sm:h-[250px] md:h-[300px]"
+                />
+                <div className="flex justify-between items-center w-full mt-3 px-2">
+                  <div className="flex">
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-orange-500" />
+                    <FaStar size={20} className="text-gray-400" />
+                    <FaStar size={20} className="text-gray-400" />
+                  </div>
+                  <FaHeart size={20} className="text-red-600" />
                 </div>
-             
-              <div>
-              <FaHeart size={20} className="text-red-600" />
               </div>
-
-              </div>
-            
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
